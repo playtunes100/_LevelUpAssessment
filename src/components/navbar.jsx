9841d42eb2b9fd2f89a2e71c5c7f4aeb9986c170
playtunes100 <playtunes100@gmail.com>
2024-12-10 11:23:41 +0200

    Fix: Nav logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import '../App.css';
 import '../index.css';
+
+
+
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
@@ -9,8 +12,8 @@ const Navbar = () => {
         <nav className='sticky top-0 left-0 right-0 bg-black md:px-12 px-4 micro-5 md:text-3xl text-lg'>
             <div className="border-b border-white flex md:justify-center py-4 justify-between">
                 <div className="md:w-1/2 w-auto h-16 flex items-center">
-                    <img src="../assets/logos/level-up-logo.webp" className="h-3/4 md:block hidden" alt="website nav logo" />
-                    <img src="../assets/logos/level-up-logo_mini.webp" className="h-3/4 md:hidden block" alt="website nav logo mobile"/>
+                    <img src="../_LevelUpAssessment/assets/logos/level-up-logo.webp" className="h-3/4 md:block hidden" alt="website nav logo" />
+                    <img src="../_LevelUpAssessment/assets/logos/level-up-logo_mini.webp" className="h-3/4 md:hidden block" alt="website nav logo mobile"/>
                 </div>
                 <div className="md:w-1/2 w-auto h-16 flex md:gap-16 gap-10 items-center justify-end uppercase">
                     <NavLink 
@@ -23,7 +26,7 @@ const Navbar = () => {
                         Home
                     </NavLink>
                     <NavLink 
-                        to={'/viewall'}
+                        to={'/_LevelUpAssessment/viewall'}
                         className={({ isActive }) =>
                             [
                             isActive ? "text-orange" : "text-white",
@@ -32,7 +35,7 @@ const Navbar = () => {
                         Graduate List
                     </NavLink>
                     <NavLink 
-                    to={'/create'}
+                    to={'/_LevelUpAssessment/create'}
                     className={({ isActive }) =>
                         [
                         isActive ? "text-orange" : "text-white",
