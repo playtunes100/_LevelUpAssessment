794d7ef4a4532fb070c56694854e96d4cc7ed8af
playtunes100 <playtunes100@gmail.com>
2024-12-11 23:19:16 +0200

    Updates

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -15,7 +15,7 @@ const Listitem= (props) =>{
             <td className="md:py-4 py-2 md:px-8 px-4">{props.firstName } {props.lastName}</td>
             <td className="md:py-4 py-2 md:px-8 px-4 md:block hidden">{props.emailAddress ? props.emailAddress : props.phoneNumber ? props.phoneNumber : "Field Empty"}</td>
             <td className=" md:py-4 py-2 md:px-8 px-4 gap-4">
-                <div className="flex justify-around">
+                <div className="flex justify-around flex-col md:flex-row">
                     <Link to={'../customer/'+ props.graduateId} ><button className="inline rounded-full border-2 border-orange text-orange text-sm p-2 pl-4 pr-4 uppercase">View Mode</button></Link>
                     <Link to={'../update/'+ props.graduateId} ><button className="inline rounded-full border-2 border-green text-green text-sm p-2 pl-4 pr-4 uppercase">Update</button></Link>
                     
